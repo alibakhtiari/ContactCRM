354d8f6805a74e33144a2b5048a5e2956b1159a0
Ali Bakhtiari <ali.bakhtiarii@gmail.com>
2025-11-06 00:33:33 +0330

    update to expo 54

--- android/app/src/main/java/com/hero2003/RamzArzNegaran/MainApplication.kt
+++ android/app/src/main/java/com/hero2003/RamzArzNegaran/MainApplication.kt
@@ -8,9 +8,10 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactHost
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.common.ReleaseLevel
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
 import com.hero2003.CallLogModule.CallLogPackage
@@ -23,12 +24,12 @@ class MainApplication : Application(), ReactApplication {
         this,
         object : DefaultReactNativeHost(this) {
           override fun getPackages(): List<ReactPackage> {
-            val packages = PackageList(this).packages
-            // Add our custom package
-            packages.add(CallLogPackage())
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(MyReactNativePackage())
-            return packages
+            return PackageList(this).packages.apply {
+              // Add our custom package
+              add(CallLogPackage())
+              // Packages that cannot be autolinked yet can be added manually here, for example:
+              // add(MyReactNativePackage())
+            }
           }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
@@ -45,11 +46,12 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+    try {
+      DefaultNewArchitectureEntryPoint.releaseLevel = ReleaseLevel.STABLE
+    } catch (e: Exception) {
+      // Handle exception if needed
     }
+    loadReactNative(this)
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 
